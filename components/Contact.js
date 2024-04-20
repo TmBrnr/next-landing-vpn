72142920a6cd5a9074253dbcebc3c1445814dd50
s82626 <timboerner78@gmail.com>
2024-04-20 17:39:09 +0200

    readded speed insights

--- components/Contact.js
+++ components/Contact.js
@@ -83,7 +83,8 @@ export default function Contact() {
                 name="message"
                 placeholder="Your Message"
                 required
-                className="text-black-500 min-h-[300px] w-full mb-4 px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500 transition-colors"
+                className="text-black-500 w-full mb-4 px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500 transition-colors"
+                style={{ minHeight: "300px" }}
               ></textarea>
               <ButtonPrimary type="submit">Submit Form</ButtonPrimary>
             </form>
--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 const Layout = ({ children }) => {
@@ -8,6 +9,7 @@ const Layout = ({ children }) => {
     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       
       <Header />
+      <SpeedInsights />
       <main style={{ flexGrow: 1 }}>{children}</main>
       <Footer />
     </div>
