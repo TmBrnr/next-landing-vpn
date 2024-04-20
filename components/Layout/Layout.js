ee7edd5f5e85ae1024d4540850a3f29ee348ecea
s82626 <timboerner78@gmail.com>
2024-04-20 15:54:29 +0200

    added speed insights

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 const Layout = ({ children }) => {
   return (
     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      
       <Header />
+      <SpeedInsights />
       <main style={{ flexGrow: 1 }}>{children}</main>
       <Footer />
     </div>
